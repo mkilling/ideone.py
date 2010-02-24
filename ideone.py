e15f1ce8857bcf7d02b6c6235dcfe355917a9b36
mkilling <marvinkilling@gmx.de>
2010-02-24 23:00:40 +0100

    test function and getLanguages() working

--- ideone.py
+++ ideone.py
@@ -1,5 +1,16 @@
 from SOAPpy import WSDL
 
+
+def createTuple(item):
+    return (item['key'], item['value']) 
+
+def createDict(items):
+    ret = {}
+    tuples = map(createTuple, items)
+    for tuple in tuples:
+        ret[tuple[0]] = tuple[1]
+    return ret
+
 class IdeOne:
     def __init__(self, user='test', password='test'):
         self._user = user
@@ -8,21 +19,22 @@ class IdeOne:
         # creating wsdl client
         self._wsdlObject = WSDL.Proxy('http://ideone.com/api/1/service.wsdl')
 
+
     def getLanguages(self):
         """returns a list of tuples with languages supported by ideone"""
         response = self._wsdlObject.getLanguages(self._user, self._password)
-        languages = response[0][1][1][0]
+        languages = response['item'][1]['value']['item']
 
-        print response['item']
-
-        createTuple = lambda l: (l['key'], l['value']) 
         return map(createTuple, languages)
 
     def testFunction(self):
         """call ideone test function"""
         result = self._wsdlObject.testFunction('test', 'test')
+        items = result['item']
+        return createDict(items)
 
 
 if __name__ == "__main__":
     ideone = IdeOne()
-    ideone.getLanguages()
+    print ideone.getLanguages()
+    print ideone.testFunction()
