7912b260d1ba518d0c6f9985b86e9eb6380fec3b
mkilling <marvinkilling@gmx.de>
2010-02-24 23:09:50 +0100

    createSubmission basically works

--- ideone.py
+++ ideone.py
@@ -15,26 +15,30 @@ class IdeOne:
     def __init__(self, user='test', password='test'):
         self._user = user
         self._password = password
-
-        # creating wsdl client
         self._wsdlObject = WSDL.Proxy('http://ideone.com/api/1/service.wsdl')
 
+    def testFunction(self):
+        """call ideone test function"""
+        response = self._wsdlObject.testFunction('test', 'test')
+        items = response['item']
+        return createDict(items)
 
     def getLanguages(self):
         """returns a list of tuples with languages supported by ideone"""
         response = self._wsdlObject.getLanguages(self._user, self._password)
         languages = response['item'][1]['value']['item']
-
         return map(createTuple, languages)
 
-    def testFunction(self):
-        """call ideone test function"""
-        result = self._wsdlObject.testFunction('test', 'test')
-        items = result['item']
-        return createDict(items)
+    def createSubmission(self, code, language, input='', run=True, private=False):
+        response = self._wsdlObject.createSubmission(self._user, self._password, code, language, input, run, private)
+        link = response['item'][1]['value']
+        return link
+
 
 
 if __name__ == "__main__":
+    PYTHON = 116
     ideone = IdeOne()
     print ideone.getLanguages()
     print ideone.testFunction()
+    print ideone.createSubmission("print 'Hello World'\n", PYTHON)
